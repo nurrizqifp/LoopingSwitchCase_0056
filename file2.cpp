f155dbf1c7ca3c4f231226b273a6c09cee0d0f45
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-03-18 15:56:27 +0700

    Membuat Perulangan While

---
+++ file2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main () {
+    //perulangan while
+    cout << "Perulangan While" << endl;
+    int x;
+    int perulanganWhile = 0;
+    int perulanganDoWhile = 0;
+    srand(time(0));
+    x = rand() % 10;
+    
+    cout << "Nilai x awal : " << x << endl;
+    while(x < 7){
+        cout << "Nilai x : " << x << " Lari Keliling Lapangan" << endl;
+        x = rand() % 10;
+        perulanganWhile++;
+    }
+}
