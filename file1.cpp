406f9f9e26bcc1bd4becc965425daa6d2cfe181c
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-03-18 15:42:57 +0700

    Membuat Perulangan for pencacah naik

---
+++ file1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //Perulangan for untuk pencacah naik
+    int i;
+    cout << "Perulangan For Pencacah Naik" << endl;
+    for(i= 0 ; i <= 4; i++){
+        cout << "Nilai i : " << i
+        << " Lari keliling lapangan" << endl;
+    }
+
+    cout << "Nilai i terakhir : " << i << endl;
+    cout << endl;
+}
