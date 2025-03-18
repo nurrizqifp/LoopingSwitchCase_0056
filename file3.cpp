bc54a9c0b92e28e083cf6e783c4addb9396fc41c
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-03-18 16:51:21 +0700

    Membuat menu dengan do while pada program utama

--- file3.cpp
+++ file3.cpp
@@ -9,4 +9,17 @@ float LuasLingkaran(float r){
     return 3.14 * r * r;
 }
 
-int main(){}
+int main(){
+    int pilihan;
+    float panjang, lebar, jejari;
+
+    do{
+        cout << "===============Menu===============" << endl;
+        cout << "1. Menghitung Luas Persegi Panjang" << endl;
+        cout << "2. Menghitung Luas Lingkaran" << endl;
+        cout << "3. Exit" << endl;
+        cout << "==================================" << endl;
+        cout << "Masukkan Pilihan (1/2/3) : ";
+        cin >> pilihan;
+    } while (pilihan != 3);
+}
