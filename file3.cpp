7b509adf98f7a9b3f732b780310f4ed54718cdf0
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-03-18 16:54:51 +0700

    Menambahkan switch pilihan

--- file3.cpp
+++ file3.cpp
@@ -21,5 +21,30 @@ int main(){
         cout << "==================================" << endl;
         cout << "Masukkan Pilihan (1/2/3) : ";
         cin >> pilihan;
+
+        switch(pilihan) {
+            case 1 :
+                cout << "Menghitung Luas Persegi Panjang" << endl;
+                cout << "Masukkan Panjang : ";
+                cin >> panjang;
+                cout << "Masukkan Lebar : ";
+                cin >> lebar;
+                cout << "Luas Persegi Panjang : " <<
+                LuasPersegiPanjang(panjang, lebar) << endl;
+                break;
+            case 2 :
+                cout << "Menghitung Luas Lingkaran" << endl;
+                cout << "Masukkan Jari-jari : ";
+                cin >> jejari;
+                cout << "Luas Lingkaran : " << LuasLingkaran(jejari) << endl;
+                break;
+            case 3 :
+                cout << "Maturnuwun :D";
+                break;
+            default :
+                cout << "Pilihan anda salah!";
+                cout << endl;
+                break;
+        }
     } while (pilihan != 3);
 }
